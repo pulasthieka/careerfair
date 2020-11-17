01ec5a7e1bf1060932c1e2f6843d5642dccd1956
pulasthieka <pulasthieka@yahoo.com>
2020-11-17 23:58:51 +0530

    Version integration and cleanup

--- careerfair2020/src/environments/environment.ts
+++ careerfair2020/src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAfRFpJsD0NxeaXD54siLMmz0K7mNq11bE",
-    authDomain: "careerfair-entc.firebaseapp.com",
-    databaseURL: "https://careerfair-entc.firebaseio.com",
-    projectId: "careerfair-entc",
-    storageBucket: "careerfair-entc.appspot.com",
-    messagingSenderId: "531533423505",
-    appId: "1:531533423505:web:add39162bda13627717030",
-    measurementId: "G-RRMK662FV7"
-  }
+    apiKey: 'AIzaSyAfRFpJsD0NxeaXD54siLMmz0K7mNq11bE',
+    authDomain: 'careerfair-entc.firebaseapp.com',
+    databaseURL: 'https://careerfair-entc.firebaseio.com',
+    projectId: 'careerfair-entc',
+    storageBucket: 'careerfair-entc.appspot.com',
+    messagingSenderId: '531533423505',
+    appId: '1:531533423505:web:add39162bda13627717030',
+    measurementId: 'G-RRMK662FV7',
+  },
 };
 
 /*
