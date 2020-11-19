440ef50e5224cc5dfe86fa4ce5bd4c43076b7fdd
channajayanath <channajayanath.cj@gmail.com>
2020-11-20 01:19:49 +0530

    panel-table v1 completed

--- careerfair2020/src/app/panel/panel-table/panel-table.component.ts
+++ careerfair2020/src/app/panel/panel-table/panel-table.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelTableComponent implements OnInit {
 
+  applicants = [
+    {
+      name : "Jacob Alhas",
+      resume_url : "http://abd.cdm",
+      panel: "panel 2",
+      status: "Available"
+    },
+    {
+      name : "Suwal Bas",
+      resume_url : "http://abd.cdm",
+      panel: "panel 1",
+      status: "In interview"
+    },
+    {
+      name : "Lenka Duma",
+      resume_url : "http://abd.cdm",
+      panel: "panel 4",
+      status: "Hired"
+    },
+    {
+      name : "Wol Bala",
+      resume_url : "http://abd.cdm",
+      panel: "panel 3",
+      status: "Not interested"
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
