f71e066a1bdb1ccd55a890cb170260b6bc82e39a
pulasthieka <pulasthieka@yahoo.com>
2020-11-20 07:14:07 +0530

    Fixed errors

--- careerfair2020/src/app/panel/panel-table/panel-table.component.ts
+++ careerfair2020/src/app/panel/panel-table/panel-table.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import Applicants from '../../../assets/applicants''
+import Applicants from '../../../assets/applicants';
 
 @Component({
   selector: 'app-panel-table',
   templateUrl: './panel-table.component.html',
-  styleUrls: ['./panel-table.component.css']
+  styleUrls: ['./panel-table.component.css'],
 })
 export class PanelTableComponent implements OnInit {
-
   applicants = Applicants;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
