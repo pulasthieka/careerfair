390196b82f2fc0c003bbb8c68ee891300523212f
pulasthieka <pulasthieka@yahoo.com>
2020-11-28 10:30:21 +0530

    Move add new applicant to seperate component

---
+++ careerfair2020/src/app/cordinator/add-new/add-new.component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ApplicantsService } from 'src/app/services/applicants.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-add-new',
+  templateUrl: './add-new.component.html',
+  styleUrls: ['./add-new.component.css'],
+})
+export class AddNewComponent implements OnInit {
+  newIndex = '';
+  @Input() applicants;
+  @Input() company;
+  constructor(
+    private applicantService: ApplicantsService,
+    private firestore: AngularFirestore
+  ) {}
+
+  ngOnInit(): void {}
+
+  addCandidate(): void {
+    const index = this.newIndex.trim().toUpperCase();
+    console.log(this.applicants);
+    const selected = this.applicants.findIndex(
+      (el) => el.applicant_id === index
+    );
+    if (index.match(/1[0-9]{5}[A-Z]/g)) {
+      if (selected === -1) {
+        this.applicantService.getProfile(index).subscribe(
+          (res) => {
+            if (res) {
+              const applicant = {
+                applicant_id: index,
+                comment: '',
+                panel_id: '',
+                resume_url: res.default_resume || '',
+                status: 'Interested',
+              };
+
+              this.firestore
+                .collection(environment.CompanyCollection)
+                .doc(this.company)
+                .collection('applicants')
+                .doc(index)
+                .set(applicant);
+            } else {
+              alert('Index does not exist');
+            }
+          },
+          (err) => {
+            alert('Something went wrong');
+          }
+        );
+      } else {
+        alert('Already in List');
+      }
+    } else {
+      alert('Incorrect Index Format');
+    }
+  }
+}
