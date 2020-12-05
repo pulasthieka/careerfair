4da19fe64372bb33b66c69896867610ee587a9f8
pulasthieka <pulasthieka@yahoo.com>
2020-12-05 17:35:24 +0530

    Order applicants by a given order

--- careerfair2020/src/app/models/applicant.model.ts
+++ careerfair2020/src/app/models/applicant.model.ts
@@ -1,6 +1,7 @@
 export interface Applicant {
   applicant_id: string; // identifier
   comment?: string;
+  order: number;
   panel_id: string; // interviewed panel identifier
   resume_url: string; // relative to root
   status: 'Not Interested' | 'Interviewed' | 'To Interview' | 'Hired';
--- firebase/addDataToFirebase.js
+++ firebase/addDataToFirebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 const { Firestore } = require("@google-cloud/firestore");
-var serviceAccount = require("./careerfair-entc-firebase-adminsdk-yx36z-e254f56f9c.json");
+var serviceAccount = require("./careerfair-entc-firebase-adminsdk-yx36z-b2e54410c2.json");
 const fs = require("fs");
 var crud = require("./fireAdminOperations");
 admin.initializeApp({
@@ -10,9 +10,9 @@ admin.initializeApp({
 const fire = admin.firestore();
 // running functions
 // crud.deleteAllUsers();
-crud.listUsers();
+// crud.listUsers();
 // addData("students.json", "Students");
-addData("panels.json", "Panels");
+// addData("panels.json", "Panels");
 // addUsers("panels.json", "Panels");
 // addUsers("coordinator.json", "Coordinators");
 // addCompanies("companies.json", "Companies");
